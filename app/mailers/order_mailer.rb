e497274441fe64c1e36a655ac0f74c4eb4f6f7e3
Brenton Holswich <brenton.holswich@gmail.com>
2017-04-14 11:18:02 -0700

    updated cart stripe order - added email - made sense out of the cents

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,11 +1,12 @@
 class OrderMailer < ApplicationMailer
 
   def order_email(order)
-    @order = Order.find(order.id)
+    # @order = Order.find(order.id)
+    @order = order
     @user = User.find_by(email: @order.email)
     @items = @order.line_items
     to_with_name = %("#{@user.first_name} #{@user.last_name}" <#{@user.email}>)
-    mail(to: to_with_name, subject: "Order number #{order_id} confirmation")
+    mail(to: to_with_name, subject: "Order number #{@order.id} confirmation")
   end
 
 end
